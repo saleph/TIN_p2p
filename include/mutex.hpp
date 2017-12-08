15bf1bf6c014c1821fbe7c616d92d401563e292c
kolo <bkkolodziejski@gmail.com>
2017-12-08 23:14:28 +0100

    create mutex&guard

---
+++ include/mutex.hpp
@@ -0,0 +1,18 @@
+#include <pthread.h>
+
+#ifndef INCLUDE_MUTEX_HPP_
+#define INCLUDE_MUTEX_HPP_
+
+
+class Mutex {
+	pthread_mutex_t mutex;
+
+public:
+	Mutex();
+	void lock();
+	void unlock();
+	~Mutex();
+};
+
+
+#endif /* INCLUDE_MUTEX_HPP_ */
---
+++ src/library_src/mutex.cpp
@@ -0,0 +1,17 @@
+#include "mutex.hpp"
+
+Mutex::Mutex() {
+	pthread_mutex_init(&mutex, NULL);
+}
+
+void Mutex::lock() {
+	pthread_mutex_lock(&mutex);
+}
+
+void Mutex::unlock() {
+	pthread_mutex_unlock(&mutex);
+}
+
+Mutex::~Mutex() {
+	pthread_mutex_destroy(&mutex);
+}
