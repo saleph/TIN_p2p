7aa6c04f89884a8e7d3b41b7fc15343c206dff7c
Spinaker1 <klitschko456@gmail.com>
2018-01-16 06:26:37 +0100

    protocol update

--- src/library_src/ProtocolManager.cpp
+++ src/library_src/ProtocolManager.cpp
@@ -71,7 +71,12 @@ void p2p::util::initProcessingFunctions() {
     };
 
     messageProcessors[MessageType::HELLO_REPLY] = [] (const uint8_t *data, uint32_t size, in_addr_t sourceAddress) {
+        std::vector<FileDescriptor> buffer = {};
+        buffer.resize(size/sizeof(FileDescriptor),FileDescriptor(nullptr));
 
+        memcpy(buffer.data(), data, size);
+
+        networkDescriptors.insert(networkDescriptors.end(), buffer.begin(), buffer.end());
     };
 
     messageProcessors[MessageType::DISCONNECTING] = [] (const uint8_t *data, uint32_t size, in_addr_t sourceAddress) {
@@ -87,19 +92,37 @@ void p2p::util::initProcessingFunctions() {
     };
 
     messageProcessors[MessageType::NEW_FILE] = [] (const uint8_t *data, uint32_t size, in_addr_t sourceAddress) {
+        FileDescriptor newFileDescriptor(nullptr);
+        memcpy(&newFileDescriptor, data, sizeof(FileDescriptor));
 
+        networkDescriptors.push_back(newFileDescriptor);
     };
 
     messageProcessors[MessageType::REVOKE_FILE] = [] (const uint8_t *data, uint32_t size, in_addr_t sourceAddress) {
+        FileDescriptor revokedFileDescriptor(nullptr);
+        memcpy(&revokedFileDescriptor, data, sizeof(FileDescriptor));
 
+        for (std::vector<FileDescriptor>::const_iterator i = networkDescriptors.begin(); i != networkDescriptors.end(); ++i) {
+            if (i->getName() == revokedFileDescriptor.getName()) {
+                networkDescriptors.erase(i);
+                break;
+            }
+        }
     };
 
     messageProcessors[MessageType::DISCARD_DESCRIPTOR] = [] (const uint8_t *data, uint32_t size, in_addr_t sourceAddress) {
-
     };
 
     messageProcessors[MessageType::UPDATE_DESCRIPTOR] = [] (const uint8_t *data, uint32_t size, in_addr_t sourceAddress) {
+        FileDescriptor updatedFileDescriptor(nullptr);
+        memcpy(&updatedFileDescriptor, data, sizeof(FileDescriptor));
 
+        for (int i=0; i<networkDescriptors.size(); i++) {
+            if (networkDescriptors[i].getName() == updatedFileDescriptor.getName()) {
+                networkDescriptors[i] = updatedFileDescriptor;
+                break;
+            }
+        }
     };
 
     messageProcessors[MessageType::HOLDER_CHANGE] = [] (const uint8_t *data, uint32_t size, in_addr_t sourceAddress) {
@@ -107,19 +130,75 @@ void p2p::util::initProcessingFunctions() {
     };
 
     messageProcessors[MessageType::FILE_TRANSFER] = [] (const uint8_t *data, uint32_t size, in_addr_t sourceAddress) {
+        FileDescriptor fileDescriptor(nullptr);
+        memcpy(&fileDescriptor, data, sizeof(FileDescriptor));
+
 
     };
 
     messageProcessors[MessageType::UPLOAD_FILE] = [] (const uint8_t *data, uint32_t size, in_addr_t sourceAddress) {
+        FileDescriptor newFileDescriptor(nullptr);
+        memcpy(&newFileDescriptor, data, sizeof(FileDescriptor));
 
+        localDescriptors.push_back(newFileDescriptor);
+
+        //TODO - obsluga pliku
+
+        P2PMessage message = {};
+        message.setMessageType(MessageType::NEW_FILE);
+        message.setAdditionalDataSize(sizeof(FileDescriptor));
+
+        std::vector<uint8_t> buffer(sizeof(FileDescriptor) + sizeof(P2PMessage));
+
+        memcpy(buffer.data(), &message, sizeof(P2PMessage));
+        memcpy(buffer.data() + sizeof(P2PMessage), &newFileDescriptor,sizeof(FileDescriptor));
+
+        udpServer->broadcast(buffer.data(),buffer.size());
     };
 
     messageProcessors[MessageType::GET_FILE] = [] (const uint8_t *data, uint32_t size, in_addr_t sourceAddress) {
+        FileDescriptor fileDescriptor(nullptr);
+        memcpy(&fileDescriptor, data, sizeof(FileDescriptor));
 
+        P2PMessage message = {};
+        message.setMessageType(MessageType::FILE_TRANSFER);
+        message.setAdditionalDataSize(sizeof(FileDescriptor)+fileDescriptor.getSize());
+
+        std::vector<uint8_t> buffer(sizeof(FileDescriptor) + sizeof(P2PMessage) + fileDescriptor.getSize());
+        memcpy(buffer.data(), &message, sizeof(P2PMessage));
+        memcpy(buffer.data() + sizeof(P2PMessage), &fileDescriptor, sizeof(FileDescriptor));
+        /*
+        FILE *stream = fopen(fileDescriptor.getName(), "rb");
+        fread(void *ptr, size_t size, size_t nitems, FILE *stream);
+        */
+
+        tcpServer->sendData(buffer.data(),buffer.size(),sourceAddress);
     };
 
     messageProcessors[MessageType::DELETE_FILE] = [] (const uint8_t *data, uint32_t size, in_addr_t sourceAddress) {
+        FileDescriptor deletedFileDescriptor(nullptr);
+        memcpy(&deletedFileDescriptor, data, sizeof(FileDescriptor));
 
+        for (std::vector<FileDescriptor>::const_iterator i = localDescriptors.begin(); i != localDescriptors.end(); ++i) {
+            if (i->getName() == deletedFileDescriptor.getName()) {
+                localDescriptors.erase(i);
+                break;
+            }
+        }
+
+        //TODO - obsluga pliku
+
+
+        P2PMessage message = {};
+        message.setMessageType(MessageType::REVOKE_FILE);
+        message.setAdditionalDataSize(sizeof(FileDescriptor));
+
+        std::vector<uint8_t> buffer(sizeof(FileDescriptor) + sizeof(P2PMessage));
+
+        memcpy(buffer.data(), &message, sizeof(P2PMessage));
+        memcpy(buffer.data() + sizeof(P2PMessage), &deletedFileDescriptor,sizeof(FileDescriptor));
+
+        udpServer->broadcast(buffer.data(),buffer.size());
     };
 
 }
